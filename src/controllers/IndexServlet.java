a40ca63f4b7b4c27bc6b77c91896054a321e84c5
shunsuke.iwata <affili8shunsuke@gmail.com>
2022-11-15 18:05:07 +0900

    CreateServlet.javaを作成

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -19,7 +19,7 @@ import utils.DBUtil;
  */
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -29,10 +29,9 @@ public class IndexServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
